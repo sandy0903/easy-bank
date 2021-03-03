c8296ae17f913f71d3596e49c40061f2b74b0269
duyên huynh <dh18062000@gmail.com>
2021-03-03 16:15:04 +0700

    first commit

---
+++ javascript.js
@@ -0,0 +1,7 @@
+var menu= document.getElementById("menu").addEventListener("click",showNav);;
+var nav= document.getElementById("nav-bar");
+
+function showNav(e){
+    
+    //nav.style.display="block"
+}
